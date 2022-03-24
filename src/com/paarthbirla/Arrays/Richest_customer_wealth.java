968dfac007e753a3342fe9bc574b02b719ba12ef
Paarth Birla <birlapaarth12@gmail.com>
2022-03-24 18:43:58 +0530

    Richest Customer Wealth

--- src/com/paarthbirla/Arrays/Richest_customer_wealth.java
+++ src/com/paarthbirla/Arrays/Richest_customer_wealth.java
@@ -1,5 +1,14 @@
 package com.paarthbirla.Arrays;
 
+/*
+Question:
+        You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the
+        ith customer has in the jth bank. Return the wealth that the richest customer has.
+
+        A customer's wealth is the amount of money they have in all their bank accounts.
+        The richest customer is the customer that has the maximum wealth.
+*/
+
 public class Richest_customer_wealth {
 
     public static int maximumWealth(int[][] accounts)
