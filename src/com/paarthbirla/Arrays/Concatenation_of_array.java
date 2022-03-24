d1ecb2fa3e09c84d78e3315eef4f04aa82251c96
Paarth Birla <birlapaarth12@gmail.com>
2022-03-24 18:32:03 +0530

    Concatenation of Arrays

--- src/com/paarthbirla/Arrays/Concatenation_of_array.java
+++ src/com/paarthbirla/Arrays/Concatenation_of_array.java
@@ -2,6 +2,16 @@ package com.paarthbirla.Arrays;
 
 import java.util.Arrays;
 
+/*
+Question:
+        Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and
+        ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+        Specifically, ans is the concatenation of two nums arrays.
+
+        Return the array ans.
+*/
+
 public class Concatenation_of_array {
     public static int[] concatenate(int[] nums)
     {
